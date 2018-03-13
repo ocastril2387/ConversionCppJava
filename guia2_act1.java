c3d16e301f1549e94711277498efe6c28d730977
ocastril2387 <ocastril2387@universidadean.edu.co>
2018-03-13 15:04:44 -0500

    Definición de los miembros de la clase Dupla.

--- guia2_act1.java
+++ guia2_act1.java
@@ -1,6 +1,36 @@
 public class Dupla{
 	
+	private int a;
+	private int b;
 	
+	public Dupla(){
+	}
+	
+	public Dupla(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int getA(){
+		return a;
+	}
+	
+	public void setA(int a){
+		this.a = a;
+	}
+	
+	public int getB(){
+		return b;
+	}
+	
+	public void setB(int b){
+		this.b = b;
+	}
+	
+	public void guardar(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
 	
 	public static void main(String[] args){
 		
